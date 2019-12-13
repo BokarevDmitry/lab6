213a76f88243def897e1231f0ece30ff272b20ce
BokarevDmitry <dbokarevv@gmail.com>
2019-12-13 15:03:08 +0300

    Scripted auto-commit on change (2019-12-13 15:03:08) by gitwatch.sh

--- src/main/java/bokarev/ReturnMessage.java
+++ src/main/java/bokarev/ReturnMessage.java
@@ -1,4 +1,13 @@
 package bokarev;
 
 public class ReturnMessage {
+    private String server;
+
+    public ReturnMessage(String server) {
+        this.server = server;
+    }
+
+    public String getServer() {
+        return server;
+    }
 }
