908a5367925450e6c85b1c940dc3912d8c520c09
BokarevDmitry <dbokarevv@gmail.com>
2019-12-13 21:00:05 +0300

    Scripted auto-commit on change (2019-12-13 21:00:05) by gitwatch.sh

--- src/main/java/bokarev/StorageActor.java
+++ src/main/java/bokarev/StorageActor.java
@@ -39,8 +39,10 @@ public class StorageActor extends AbstractActor {
                 })
                 .match(GetMessage.class, m -> {
                     log.info("GETMESSAGE REQUEST");
+                    Integer a = random.nextInt(storage.size());
+                    if (a==0) a+=1;
                     System.out.println("storage-size = " + storage.size());
-                    getSender().tell(new ReturnMessage(storage.get(random.nextInt(storage.size()))),
+                    getSender().tell(new ReturnMessage(storage.get(a)),
                             ActorRef.noSender());
 
                 })
