536c9db5fda595f6d2647b3d5728acc9095582e4
BokarevDmitry <dbokarevv@gmail.com>
2019-12-13 14:44:44 +0300

    Scripted auto-commit on change (2019-12-13 14:44:44) by gitwatch.sh

--- src/main/java/bokarev/AnonymousServer.java
+++ src/main/java/bokarev/AnonymousServer.java
@@ -1,4 +1,15 @@
 package bokarev;
 
+import akka.actor.ActorRef;
+import org.apache.zookeeper.ZooKeeper;
+import org.asynchttpclient.AsyncHttpClient;
+
 public class AnonymousServer {
+    private static String URL = "url";
+    private static String COUNT = "count";
+    static AsyncHttpClient asyncHttpClient;
+    static ActorRef storageActor;
+    static ZooKeeper zooKeeper;
+
+    public AnonymousServer()
 }
