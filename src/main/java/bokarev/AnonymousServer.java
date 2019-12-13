353ba3281ae9be8962dc416e1e7ff2ed2cae97f4
BokarevDmitry <dbokarevv@gmail.com>
2019-12-13 15:02:00 +0300

    Scripted auto-commit on change (2019-12-13 15:02:00) by gitwatch.sh

--- src/main/java/bokarev/AnonymousServer.java
+++ src/main/java/bokarev/AnonymousServer.java
@@ -2,16 +2,27 @@ package bokarev;
 
 import akka.actor.ActorRef;
 import akka.http.impl.engine.client.PoolConductor;
+import akka.pattern.Patterns;
 import akka.routing.RouterActor;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
 import org.asynchttpclient.AsyncHttpClient;
+import akka.http.javadsl.server.Route;
+import org.asynchttpclient.Request;
+import org.asynchttpclient.Response;
+
+import java.net.ConnectException;
+import java.time.Duration;
+import java.util.concurrent.CompletionStage;
+
+import static akka.http.javadsl.server.Directives.*;
 
 public class AnonymousServer {
     private static String URL = "url";
     private static String COUNT = "count";
-    AsyncHttpClient asyncHttpClient;
-    ActorRef storageActor;
-    ZooKeeper zooKeeper;
+    static AsyncHttpClient asyncHttpClient;
+    static ActorRef storageActor;
+    static ZooKeeper zooKeeper;
 
     public AnonymousServer(ActorRef storageActor, AsyncHttpClient asyncHttpClient, ZooKeeper zooKeeper) {
         this.asyncHttpClient = asyncHttpClient;
@@ -19,7 +30,50 @@ public class AnonymousServer {
         this.zooKeeper = zooKeeper;
     }
 
-    public static Route
+    public static Route createRoute() {
+        return route (
+                get (() -> parameter(URL, url ->
+                            parameter(COUNT, count ->
+                            )))
+        );
+    }
+
+    public static Route handleGetRequest (String url, Integer count) {
+        CompletionStage<Response> res;
+        if (count == 0) {
+            res = fetch(asyncHttpClient.prepareGet(url).build());
+        } else {
+            res = Redirect(url, count--);
+        }
+        return completeOKWithFutureString(res.thenApply(Response::getResponseBody));
+    }
+
+    public static  CompletionStage<Response> Redirect(String url, Integer count) {
+        return Patterns.ask(storageActor, new GetMessage(), Duration.ofMillis(3000))
+                .thenApply(o -> ((ReturnMessage)o))
+    }
+
+    public static CompletionStage<Response> fetch(Request req) {
+        return asyncHttpClient.executeRequest(req).toCompletableFuture();
+    }
+
+    private static Response BadDirection (Response res, Throwable ex, String z) {
+        if (ex instanceof ConnectException) {
+            storageActor.tell(new DeleteMessage(), ActorRef.noSender());
+        }
+        return  res;
+    }
+
+    private static String getServerUrl(String node) throws KeeperException, InterruptedException {
+        return new String(zooKeeper.getData(node, false, null));
+    }
+
+    public static Request createServerRequest(String serverUrl, String url, Integer count) {
+        return asyncHttpClient.prepareGet(serverUrl).addQueryParam(URL, url)
+                .addQueryParam(COUNT, Integer.toString(count)).build();
+    }
+
+
 
 
 }
