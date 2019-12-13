6c7e5f930be20d688e53a67ed6fc8cddb128ec8e
BokarevDmitry <dbokarevv@gmail.com>
2019-12-13 15:04:01 +0300

    Scripted auto-commit on change (2019-12-13 15:04:01) by gitwatch.sh

--- src/main/java/bokarev/DeleteMessage.java
+++ src/main/java/bokarev/DeleteMessage.java
@@ -1,4 +1,13 @@
 package bokarev;
 
 public class DeleteMessage {
+    private String server;
+    
+    public DeleteMessage(String server) {
+        this.server = server;
+    }
+
+    public String getServer() {
+        return server;
+    }
 }
