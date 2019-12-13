09d75c8f38179796a5cf83463833a25782e0fc21
BokarevDmitry <dbokarevv@gmail.com>
2019-12-13 15:12:26 +0300

    Scripted auto-commit on change (2019-12-13 15:12:26) by gitwatch.sh

--- src/main/java/bokarev/PutMessage.java
+++ src/main/java/bokarev/PutMessage.java
@@ -1,4 +1,15 @@
 package bokarev;
 
+import java.util.ArrayList;
+
 public class PutMessage {
+    ArrayList<String> servers;
+
+    public PutMessage(ArrayList<String> servers) {
+        this.servers = servers;
+    }
+
+    public ArrayList<String> getServers() {
+        return servers;
+    }
 }
