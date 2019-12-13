0094016c0da3b37889f091c9e6868c2714bdfb89
BokarevDmitry <dbokarevv@gmail.com>
2019-12-13 14:43:21 +0300

    Scripted auto-commit on change (2019-12-13 14:43:21) by gitwatch.sh

--- src/main/java/bokarev/App.java
+++ src/main/java/bokarev/App.java
@@ -8,27 +8,25 @@ import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
-import akka.http.javadsl.model.Query;
 import akka.http.javadsl.server.AllDirectives;
-import akka.pattern.Patterns;
+
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import org.apache.zookeeper.ZooKeeper;
 import org.asynchttpclient.AsyncHttpClient;
-import scala.Int;
-//import scala.concurrent.Future;
-import java.io.IOException;
-import java.util.Optional;
-import java.util.concurrent.CompletionStage;
-import java.util.regex.Pattern;
-//import java.util.concurrent.Future;
 
-import static akka.http.javadsl.server.Directives.complete;
-import static akka.http.javadsl.server.Directives.parameter;
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+import java.util.logging.Logger;
+
+
+
+
 import static org.asynchttpclient.Dsl.asyncHttpClient;
 
 
 public class App extends AllDirectives {
+    static Logger log = Logger.getLogger(App.class.getName());
     private static String CONN = "127.0.0.1:2181";
     private static String STRINGPATH = "/servers";
     private static String LOCALHOST = "loacalhost";
@@ -37,23 +35,25 @@ public class App extends AllDirectives {
     public static void main(String[] args) throws Exception, InterruptedException, IOException {
         String host = args[0];
         Integer port = Integer.parseInt(args[1]);
-        final ZooKeeper zoo = new ZooKeeper(CONN, TIMEOUT, err->log.info(err.toString()) )
-
+        final ZooKeeper zoo = new ZooKeeper(CONN, TIMEOUT, err->log.info(err.toString()));
         ActorSystem system = ActorSystem.create("routes");
+
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
+        AsyncHttpClient httpClient = asyncHttpClient();
+
+        ActorRef storageActor = system.actorOf (StorageActor.props(), "Storage-Actor");
 
 
-        Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
-                router.createRoute();
+
+
+        Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = router.createRoute();
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
-                ConnectHttp.toHost("localhost", 8080),
+                ConnectHttp.toHost(host, port),
                 materializer
         );
 
-        System.out.println("Server online at http://localhost:8080/");
-
-
+        System.out.println("Server online at " + host + ":" + port + "/");
     }
 }
